4f5425d7a601b9b881e80504def212c4def80146
Hank Ehly <henry.ehly@gmail.com>
2023-10-05 19:21:59 +0900

    Add lesson 12 readme

--- raspberry_pi_sandbox/12_rgb_buttons_with_pwm/main.py
+++ raspberry_pi_sandbox/12_rgb_buttons_with_pwm/main.py
@@ -108,38 +108,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# Red
-# Green
-# Blue
-# Yellow (red 100, green 100)
-# Purple (red 100, blue 100)
-
-# Turquoise:
-# Hex: #40E0D0
-# RGB Percentage: Red: 25.1%, Green: 87.84%, Blue: 81.57%
-
-# Gold:
-# Hex: #FFD700
-# RGB Percentage: Red: 100%, Green: 84.31%, Blue: 0%
-
-# Salmon: (not so much)
-# Hex: #FA8072
-# RGB Percentage: Red: 98.04%, Green: 50.98%, Blue: 45.1%
-
-# Sky Blue:
-# Hex: #87CEEB
-# RGB Percentage: Red: 52.16%, Green: 80.39%, Blue: 92.55%
-
-# Forest Green:
-# Hex: #228B22
-# RGB Percentage: Red: 13.33%, Green: 54.12%, Blue: 13.33%
-
-# Tomato: (not so much)
-# Hex: #FF6347
-# RGB Percentage: Red: 100%, Green: 38.43%, Blue: 27.84%
-
-# Peru:
-# Hex: #CD853F
-# RGB Percentage: Red: 80.39%, Green: 52.55%, Blue: 24.71%
