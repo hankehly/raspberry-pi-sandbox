c51a1142ac6a40a21ee3184676004a6f3031eda5
Hank Ehly <henry.ehly@gmail.com>
2023-10-20 18:29:34 +0900

    Comments

--- raspberry_pi_sandbox/19_servo/main.py
+++ raspberry_pi_sandbox/19_servo/main.py
@@ -30,29 +30,27 @@ def main():
     adc.setup()
 
     # Setup servo control pin
-    # GPIO.setup(args.control_pin, GPIO.OUT)
-
     # The servo expects to see a pulse every 20 ms.
     # T = 20 ms
     # f = 1/T = 50 Hz
-    # pwm = GPIO.PWM(args.control_pin, 50)
-    # pwm.start(0)
     pi = pigpio.pi()
     pi.set_mode(args.control_pin, pigpio.OUTPUT)
     pi.set_PWM_frequency(args.control_pin, 50)
     pi.set_PWM_range(args.control_pin, 1024)
 
+    # Setup rolling average calculator to smooth out the ADC readings
     calc = RollingAverageCalculator(20)
 
     try:
         while True:
-            adc_value = adc.read(channel=0)  # between 0 and 255
+            adc_value = adc.read(channel=0)
             avg_value = calc.rolling_average(adc_value)
             # Translate range 0 ~ 255 to 25 ~ 125
             duty = round(25 + (avg_value / 255) * 100)
             pi.set_PWM_dutycycle(args.control_pin, duty)
-            # pwm.ChangeDutyCycle(duty)
-            print(f"avg_value: {avg_value:.1f}, duty: {duty}")
+            print(
+                f"adc_value: {adc_value:.1f}, rolling_avg: {avg_value:.1f}, duty: {duty}"
+            )
             time.sleep(0.01)
     except KeyboardInterrupt:
         pi.stop()
